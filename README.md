5d43cf3a1d310689a9bb975a6c670d0b676dace5
James Causon <xcausxn@hotmail.co.uk>
2015-04-12 18:06:07 +0100

    Added Streams.

---
+++ README.md
@@ -0,0 +1,20 @@
+# NodeBB Twitch Plugin
+
+This NodeBB plugin allows users to embed Twitch Streams and Multi Twitch Streams inline in their posts. 
+
+## Installation
+
+    npm install nodebb-plugin-twitch
+
+## Usage
+
+    http://www.twitch.tv/CHANNEL
+
+    http://multitwitch.tv/CHANNEL/CHANNEL
+
+## Screenshots
+Twitch Single
+![twitch-plugin.png](http://i.imgur.com/Li6o3gT.png) 
+
+Multi Twitch
+![twitch-plugin.png](http://i.imgur.com/by2pOad.jpg) 
---
+++ library.js
@@ -0,0 +1,23 @@
+(function(module) {
+	"use strict";
+	var Beam = {},
+			embed = '<iframe height="280" width="500" src="https://beam.pro/embed/player/$1"></iframe>';
+	var	regularStreamUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:beam\.pro)\/(.+)">.+<\/a>/g;
+	var embedStreamUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:beam\.pro)\/(?:embed)\/(?:player)\/(.+)">.+<\/a>/g;
+
+	Beam.parse = function(data, callback) {
+		if (!data || !data.postData || !data.postData.content) {
+			return callback(null, data);
+		}
+		if (data.postData.content.match(embedStreamUrl)) {
+			data.postData.content = data.postData.content.replace(embedStreamUrl, embed);
+		}
+		if (data.postData.content.match(regularStreamUrl)) {
+			data.postData.content = data.postData.content.replace(regularStreamUrl, embed);
+		}
+
+		callback(null, data);
+	};
+
+	module.exports = Beam;
+}(module));
